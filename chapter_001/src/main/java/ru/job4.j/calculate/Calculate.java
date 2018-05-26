e541381b882768bbd5e150a11b6fc2338955f9cf
Georgyi Nishnianidze <greserve12@yandex.ru>
2018-05-27 01:46:46 +0300

    Создание JavaDoc

---
+++ chapter_001/src/main/java/ru/job4.j/calculate/Calculate.java
@@ -0,0 +1,35 @@
+package ru.job4.j.calculate;
+
+   
+/**
+    * Class ????? ??????????
+    * @author giorgyi nishnianidze
+    * @since 08.05.2018
+    * @version : 1.0
+    */
+
+public class Calculate {
+
+   /**
+    * Main.
+    * @param args - args.
+    */
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+    }
+
+        /**
+         * Method echo.
+         * @param name Georgyi.
+         * @return Echo plus Georgyi.
+         */
+        public String echo(String name) {
+            return "Echo, echo, echo :" + name;
+        }
+    }
+
+
+
+
